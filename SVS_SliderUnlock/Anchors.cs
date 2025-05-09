af92f8ad0588209c49e97246671055139cb6e8ed
y0soro <y0soro@protonmail.com>
2025-05-09 13:00:55 +0800

    feat: add new byte patterns for SVS v1.1.6

--- SVS_SliderUnlock/Anchors.cs
+++ SVS_SliderUnlock/Anchors.cs
@@ -49,26 +49,33 @@ internal class Anchors
 
     public Anchors(ManualLogSource Log, UnmanagedMemoryStream stream, long searchBegin = 4096)
     {
-        long Search(string pattern, long seekPos = -1, long assert = -1)
+        long Search(string[] patterns, long seekPos = -1, long assert = -1)
         {
-            stream.Seek(seekPos < 0 ? searchBegin : seekPos, SeekOrigin.Begin);
-            if (!new BytePattern(pattern).Search(stream, out long pos))
+            foreach (var pattern in patterns)
             {
-                throw new Exception($"patten {pattern} not found");
+                stream.Seek(seekPos < 0 ? searchBegin : seekPos, SeekOrigin.Begin);
+                if (!new BytePattern(pattern).Search(stream, out long pos))
+                {
+                    continue;
+                }
+
+                if (assert > 0)
+                {
+                    AssertAddr(assert, pos);
+                }
+
+                Log.LogDebug($"Found pattern in 0x{pos + 0x180000000:X}: {pattern}");
+                return pos;
             }
 
-            if (assert > 0)
-            {
-                AssertAddr(assert, pos);
-            }
-
-            Log.LogDebug($"Found pattern in 0x{pos + 0x180000000:X}: {pattern}");
-            return pos;
+            throw new Exception($"non of patten in {string.Join(", ", patterns)} found");
         }
 
-        // 0x180727a11 of SVS v1.1.2(same for addresses below)
         EntryUndo_CheckSliderRatio = Search(
-            "77 ?? f3 0f 10 0d ?? ?? ?? ?? 0f 2f f1 76 ?? 0f 28 f1 eb ?? 0f 57 f6 33 d2 0f 28 c6 e8",
+            [
+                // 0x180727a11 of SVS v1.1.2(same for addresses below)
+                "77 ?? f3 0f 10 0d ?? ?? ?? ?? 0f 2f f1 76 ?? 0f 28 f1 eb ?? 0f 57 f6 33 d2 0f 28 c6 e8",
+            ],
             assert: 0x180727a11
         );
 
@@ -76,49 +83,60 @@ internal class Anchors
         EntryUndo_ClampSliderRatioToOne = EntryUndo_CheckSliderRatio + 0xf;
         AssertAddr(0x180727a20, EntryUndo_ClampSliderRatioToOne);
 
-        // 0x180680518
         IsFace_BranchGreaterThanOne = Search(
-            "7f ?? ?? 8b 46 38 ?? 89 75 7f ?? 8b 00 f6 80 32 01 00 00 01 75 ?? ?? 8b c8 e8 ?? ?? ?? ?? ?? 8d 55 7f ?? 8b c8 e8",
-            assert: 0x180680518
+            [
+                // 0x18067db28 SVS v1.1.6
+                "7f ?? ?? 8b 47 38 89 75 7f ?? 8b 00 f6 80 32 01 00 00 01 75 ?? ?? 8b c8 e8 ?? ?? ?? ?? ?? 8d 55 7f ?? 8b c8 e8",
+                // 0x180680518
+                "7f ?? ?? 8b 46 38 ?? 89 75 7f ?? 8b 00 f6 80 32 01 00 00 01 75 ?? ?? 8b c8 e8 ?? ?? ?? ?? ?? 8d 55 7f ?? 8b c8 e8",
+            ]
         );
 
-        // 0x18068055b
         IsFace_BranchLessThanZero = Search(
-            "0f 85 ?? ?? ?? ?? ?? 85 ff 74 ?? ?? 8b 0d ?? ?? ?? ?? ?? 39 b1 e0 00 00 00 75 ?? e8",
-            IsFace_BranchGreaterThanOne,
-            assert: 0x18068055b
+            [
+                // 0x18067db6a SVS 1.1.16
+                "0f 85 ?? ?? ?? ?? ?? 85 f6 74 ?? ?? 8b 0d ?? ?? ?? ?? 39 b1 e0 00 00 00 75 ?? e8",
+                // 0x18068055b
+                "0f 85 ?? ?? ?? ?? ?? 85 ff 74 ?? ?? 8b 0d ?? ?? ?? ?? ?? 39 b1 e0 00 00 00 75 ?? e8",
+            ],
+            IsFace_BranchGreaterThanOne
         );
 
-        // 0x180681728
         IsBody_BranchGreaterThanOne = Search(
-            "7f ?? ?? 8b 46 38 ?? 89 65 48 ?? 8b 00 f6 80 32 01 00 00 01 75 ?? ?? 8b c8 e8 ?? ?? ?? ?? ?? 8d 55 48",
-            assert: 0x180681728
+            [
+                // 0x180681728
+                "7f ?? ?? 8b 46 38 ?? 89 65 48 ?? 8b 00 f6 80 32 01 00 00 01 75 ?? ?? 8b c8 e8 ?? ?? ?? ?? ?? 8d 55 48",
+            ]
         );
 
-        // 0x18068176b
         IsBody_BranchLessThanZero = Search(
-            "0f 85 ?? ?? ?? ?? ?? 85 f6 74 ?? ?? 8b 0d ?? ?? ?? ?? ?? 39 a1 e0 00 00 00",
-            IsBody_BranchGreaterThanOne,
-            assert: 0x18068176b
+            [
+                // 0x18068176b
+                "0f 85 ?? ?? ?? ?? ?? 85 f6 74 ?? ?? 8b 0d ?? ?? ?? ?? ?? 39 a1 e0 00 00 00",
+            ],
+            IsBody_BranchGreaterThanOne
         );
 
-        // 0x1800126b0
         IsBody_ValidateHumanDataBodyField = Search(
-            "40 53 ?? 83 ec 30 ?? 8b 1d ?? ?? ?? ?? 0f 29 74 ?? 20 0f 28 f0 f3 0f 11 4c ?? 48 f3 0f 11 54 ?? 50",
-            assert: 0x1800126b0
+            [
+                // 0x1800126b0
+                "40 53 ?? 83 ec 30 ?? 8b 1d ?? ?? ?? ?? 0f 29 74 ?? 20 0f 28 f0 f3 0f 11 4c ?? 48 f3 0f 11 54 ?? 50",
+            ]
         );
 
-        // 0x182320d00
         AnimationKeyInfo_Controller_GetInfo = Search(
-            "?? 8b c4 ?? 89 58 10 ?? 89 70 18 55 57 ?? 54 ?? 55 ?? 56 ?? 8d 68 c1 ?? 81 ec d0 00 00 00 80 3d ?? ?? ?? ?? 00",
-            assert: 0x182320d00
+            [
+                // 0x182320d00
+                "?? 8b c4 ?? 89 58 10 ?? 89 70 18 55 57 ?? 54 ?? 55 ?? 56 ?? 8d 68 c1 ?? 81 ec d0 00 00 00 80 3d ?? ?? ?? ?? 00",
+            ]
         );
 
-        // 0x182320e8c
         var call_GetInfo_DictGetItemWithKey = Search(
-            "e8 ?? ?? ?? ?? 84 c0 0f 84 ?? ?? ?? ?? ?? 83 7c ?? 18 00 0f 86 ?? ?? ?? ?? ?? 80 7c ?? 20 00 0f 57 ff f3 ?? 0f 10 1d",
-            AnimationKeyInfo_Controller_GetInfo,
-            assert: 0x182320e8c
+            [
+                // 0x182320e8c
+                "e8 ?? ?? ?? ?? 84 c0 0f 84 ?? ?? ?? ?? ?? 83 7c ?? 18 00 0f 86 ?? ?? ?? ?? ?? 80 7c ?? 20 00 0f 57 ff f3 ?? 0f 10 1d",
+            ],
+            AnimationKeyInfo_Controller_GetInfo
         );
 
         byte[] relAddrLe = new byte[4];
@@ -133,9 +151,11 @@ internal class Anchors
             call_GetInfo_DictGetItemWithKey + 5 + BitConverter.ToInt32(relAddrLe);
         AssertAddr(0x18025d5d0, GetInfo_DictGetItemWithKey);
 
-        // 0x182320ed8
         var call_GetInfo_ListGetItem = Search(
-            "e8 ?? ?? ?? ?? ?? 8b c8 ?? 85 c0 0f 84 ?? ?? ?? ?? f2 0f 10 41 14 8b 40 1c f2 0f 11 03 89 43 08 e9",
+            [
+                // 0x182320ed8
+                "e8 ?? ?? ?? ?? ?? 8b c8 ?? 85 c0 0f 84 ?? ?? ?? ?? f2 0f 10 41 14 8b 40 1c f2 0f 11 03 89 43 08 e9",
+            ],
             call_GetInfo_DictGetItemWithKey,
             assert: 0x182320ed8
         );
